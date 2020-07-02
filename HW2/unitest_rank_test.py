6a7ec0724390055e08834727cdbc10d13347b7ff
Виталий Киселев <v>
2020-07-02 19:42:03 +0300

    homework

--- HW2/unitest_rank_test.py
+++ HW2/unitest_rank_test.py
@@ -1,10 +1,10 @@
 import unittest
 
-from ..test import Rank
+from coursepy.HW2 import Rank
 
 class RankTest(unittest.TestCase):
     def test_rank(self):
-        n = int(input())
+        n = 162
         test_var = ([165, 163, 160, 160, 157, 157, 155, 154], n)
         test_result = test_var[n]
 
