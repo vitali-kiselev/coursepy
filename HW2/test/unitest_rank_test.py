496b84c4f486c0d0ac6176585b542cc2a5f32390
Виталий Киселев <v>
2020-07-02 19:39:33 +0300

    homework

--- HW2/test/unitest_rank_test.py
+++ HW2/test/unitest_rank_test.py
@@ -4,9 +4,9 @@ from ..test import Rank
 
 class RankTest(unittest.TestCase):
     def test_rank(self):
-        lst = int(input())
-        test_var = ([165, 163, 160, 160, 157, 157, 155, 154], lst)
-        test_result = test_var[lst]
+        n = int(input())
+        test_var = ([165, 163, 160, 160, 157, 157, 155, 154], n)
+        test_result = test_var[n]
 
         self.assertEqual(test_result, Rank.MyClass.rank(test_var))
 
